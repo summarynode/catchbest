a2fddb5e9bea24ebaee552465db95680b3e3abbc
root <root@erpyjun2.cafe24.com>
2017-07-04 18:41:49 +0900

    update

---
+++ day_close_price_lib.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: cp949 -*-
+
+import sys
+import pymysql
+import redis
+import time
+
+# key | code | date | open | close | high | low | volum
+
+class ClosePrice:
+
+   def __init__(self):
+      print 'init ClosePrice'
+   
+   def find(self, dataAll, sdate):
+      nbong = 0
+      ldate = long(sdate)
+      for key, value in dataAll.items():
+         for items in value:
+            fields = items.split('|')
+            if long(fields[2]) > ldate:
+               continue
+
+            nbong += 1 
+            if nbong > 0:
+               print 'close [%s] [%d] [%s]' % (fields[1], int(fields[4]), fields[2])
+               break
--- test_day.py
+++ test_day.py
@@ -3,15 +3,18 @@
 
 import day_lib
 import day_high_value_lib
+import day_close_price_lib
 
 if __name__ == "__main__":
    # init class
    day = day_lib.DayService()
    day_find_high = day_high_value_lib.HighValue()
+   day_close_price = day_close_price_lib.ClosePrice()
 
-   day.cprint(99)
    dataAll = day.loading('20170703')
-   day_find_high.find(200, dataAll)
+
+   #day_find_high.find(200, dataAll)
+   day_close_price.find(dataAll, '20170610')
 
    """
    for key, value in dataAll.items():
